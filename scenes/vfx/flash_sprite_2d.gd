8ddaee8808c4f6d952d4d42b8a3e728b35e3ad5d
Jhon <jhon1773@github.com>
2026-05-22 09:33:21 -0500

    flash arreglado

--- scenes/vfx/flash_sprite_2d.gd
+++ scenes/vfx/flash_sprite_2d.gd
@@ -2,5 +2,5 @@ extends Sprite2D
 
 func flash(start_duration: float = 0.2, end_duration: float = 0.2):
 	var tween = create_tween()
-	tween.tween_property(material,'shader_parameter/Progress', 1.0, start_duration)
-	tween.tween_property(material,'shader_parameter/Progress', 0.0, end_duration)
+	tween.tween_property(material,'shader_parameter/Parameter', 1.0, start_duration)
+	tween.tween_property(material,'shader_parameter/Parameter', 0.0, end_duration)
